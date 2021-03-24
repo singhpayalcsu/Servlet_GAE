ad11abbef28e89fbbf4e9fc41677ea05128022bf
Payal Singh <singhpayalsp@Payals-MacBook-Pro.local>
2021-03-24 16:50:25 -0700

    GAE Servlet app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Servlet_GAE
+# Servlet application running on GAE (Google App Engine) using Eclipse IDE
+
+![app running screenshot](GAE_app_running.png)
+
+![GAE dashboard](Google_dashboard.png)
+
+### GAE URL
+[https://labproject-308519.appspot.com/hello] (https://labproject-308519.appspot.com/hello)
---
+++ lab-project/src/main/java/HelloAppEngine.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+    LocalDateTime instance = LocalDateTime.now();
+    response.getWriter().print("The time is ");
+    response.getWriter().println( instance);
+
+  }
+}
